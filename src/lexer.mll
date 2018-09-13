99b22bb50dcdc5ecc67992d22855b569ddcbdeb2
Rehan MALAK <rehan.malak@inria.fr>
2018-09-13 18:56:07 +0200

    letexercise

--- src/lexer.mll
+++ src/lexer.mll
@@ -11,31 +11,15 @@ open Support.Error
 
 let reservedWords = [
   (* Keywords *)
-  ("type", fun i -> Parser.TYPE i);
-  ("inert", fun i -> Parser.INERT i);
+  ("let", fun i -> Parser.LET i);
+  ("in", fun i -> Parser.IN i);
+  ("lambda", fun i -> Parser.LAMBDA i);
   ("if", fun i -> Parser.IF i);
   ("then", fun i -> Parser.THEN i);
   ("else", fun i -> Parser.ELSE i);
   ("true", fun i -> Parser.TRUE i);
   ("false", fun i -> Parser.FALSE i);
   ("Bool", fun i -> Parser.BOOL i);
-  ("case", fun i -> Parser.CASE i);
-  ("of", fun i -> Parser.OF i);
-  ("as", fun i -> Parser.AS i);
-  ("lambda", fun i -> Parser.LAMBDA i);
-  ("let", fun i -> Parser.LET i);
-  ("in", fun i -> Parser.IN i);
-  ("fix", fun i -> Parser.FIX i);
-  ("letrec", fun i -> Parser.LETREC i);
-  ("String", fun i -> Parser.USTRING i);
-  ("unit", fun i -> Parser.UNIT i);
-  ("Unit", fun i -> Parser.UUNIT i);
-  ("timesfloat", fun i -> Parser.TIMESFLOAT i);
-  ("Float", fun i -> Parser.UFLOAT i);
-  ("succ", fun i -> Parser.SUCC i);
-  ("pred", fun i -> Parser.PRED i);
-  ("iszero", fun i -> Parser.ISZERO i);
-  ("Nat", fun i -> Parser.NAT i);
 
   (* Symbols *)
   ("_", fun i -> Parser.USCORE i);
--- src/syntax.mli
+++ src/syntax.mli
@@ -5,47 +5,21 @@ open Support.Error
 
 (* Data type definitions *)
 type ty =
-    TyVar of int * int
-  | TyId of string
-  | TyArr of ty * ty
-  | TyUnit
-  | TyRecord of (string * ty) list
-  | TyVariant of (string * ty) list
+    TyArr of ty * ty
   | TyBool
-  | TyString
-  | TyFloat
-  | TyNat
 
 type term =
-    TmTrue of info
-  | TmFalse of info
-  | TmIf of info * term * term * term
-  | TmCase of info * term * (string * (string * term)) list
-  | TmTag of info * string * term * ty
-  | TmVar of info * int * int
+    TmVar of info * int * int
+  | TmLet of info * string * term * term
   | TmAbs of info * string * ty * term
   | TmApp of info * term * term
-  | TmLet of info * string * term * term
-  | TmFix of info * term
-  | TmString of info * string
-  | TmUnit of info
-  | TmAscribe of info * term * ty
-  | TmRecord of info * (string * term) list
-  | TmProj of info * term * string
-  | TmFloat of info * float
-  | TmTimesfloat of info * term * term
-  | TmZero of info
-  | TmSucc of info * term
-  | TmPred of info * term
-  | TmIsZero of info * term
-  | TmInert of info * ty
+  | TmTrue of info
+  | TmFalse of info
+  | TmIf of info * term * term * term
 
 type binding =
     NameBind
-  | TyVarBind
   | VarBind of ty
-  | TmAbbBind of term * (ty option)
-  | TyAbbBind of ty
 
 type command =
   | Eval of info * term
@@ -66,14 +40,11 @@ val getTypeFromContext : info -> context -> int -> ty
 (* Shifting and substitution *)
 val termShift: int -> term -> term
 val termSubstTop: term -> term -> term
-val typeShift : int -> ty -> ty
-val typeSubstTop: ty -> ty -> ty
-val tytermSubstTop: ty -> term -> term
 
 (* Printing *)
 val printtm: context -> term -> unit
 val printtm_ATerm: bool -> context -> term -> unit
-val printty : context -> ty -> unit
+val printty : ty -> unit
 val prbinding : context -> binding -> unit
 
 (* Misc *)
