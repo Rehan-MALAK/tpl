84d0827814d6d02d33573febf3561ae27d1c5ac3
Rehan MALAK <rehan.malak@inria.fr>
2018-09-13 18:56:07 +0200

    fullsimple

--- src/syntax.mli
+++ src/syntax.mli
@@ -5,20 +5,47 @@ open Support.Error
 
 (* Data type definitions *)
 type ty =
-    TyArr of ty * ty
+    TyVar of int * int
+  | TyId of string
+  | TyArr of ty * ty
+  | TyUnit
+  | TyRecord of (string * ty) list
+  | TyVariant of (string * ty) list
   | TyBool
+  | TyString
+  | TyFloat
+  | TyNat
 
 type term =
-    TmVar of info * int * int
-  | TmAbs of info * string * ty * term
-  | TmApp of info * term * term
-  | TmTrue of info
+    TmTrue of info
   | TmFalse of info
   | TmIf of info * term * term * term
+  | TmCase of info * term * (string * (string * term)) list
+  | TmTag of info * string * term * ty
+  | TmVar of info * int * int
+  | TmAbs of info * string * ty * term
+  | TmApp of info * term * term
+  | TmLet of info * string * term * term
+  | TmFix of info * term
+  | TmString of info * string
+  | TmUnit of info
+  | TmAscribe of info * term * ty
+  | TmRecord of info * (string * term) list
+  | TmProj of info * term * string
+  | TmFloat of info * float
+  | TmTimesfloat of info * term * term
+  | TmZero of info
+  | TmSucc of info * term
+  | TmPred of info * term
+  | TmIsZero of info * term
+  | TmInert of info * ty
 
 type binding =
     NameBind
+  | TyVarBind
   | VarBind of ty
+  | TmAbbBind of term * (ty option)
+  | TyAbbBind of ty
 
 type command =
   | Eval of info * term
@@ -39,11 +66,14 @@ val getTypeFromContext : info -> context -> int -> ty
 (* Shifting and substitution *)
 val termShift: int -> term -> term
 val termSubstTop: term -> term -> term
+val typeShift : int -> ty -> ty
+val typeSubstTop: ty -> ty -> ty
+val tytermSubstTop: ty -> term -> term
 
 (* Printing *)
 val printtm: context -> term -> unit
 val printtm_ATerm: bool -> context -> term -> unit
-val printty : ty -> unit
+val printty : context -> ty -> unit
 val prbinding : context -> binding -> unit
 
 (* Misc *)
